7b07b0cb019fc1e6baeb538a0517de245fa1f19c
btxl <B24090221@njupt.edu.cn>
2025-12-07 17:17:00 +0800

    返回

--- mainwindow.h
+++ mainwindow.h
@@ -1,16 +1,19 @@
-// #ifndef MAINWINDOW_H
-// #define MAINWINDOW_H
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+#include "page/MiddlePage.h"
+#include "page/RankingPage.h"
+#pragma once
+
 
 #include <QMainWindow>
-#include <QMediaPlayer>
-#include <QAudioOutput>
-
 #include "page/StartPage.h"
-#include "page/SettingPage.h"
-#include "page/RankingPage.h"
 #include "page/GamePage.h"
 #include "page/MiddlePage.h"
-#include "page/DifficultyPage.h"
+#include "page/SettingPage.h"
+#include "page/RankingPage.h"
+#include <QStackedWidget>
+#include <QFile>
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -19,23 +22,20 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-private slots:  // 添加这个部分
-    void returnToMainPage();  // 添加这个槽函数
-
 private:
-    void setupConnections();
-    void setupAudio();
+    StartPage* startPage;
+    GamePage* gamePage;
+    SettingPage* settingPage;
+    RankingPage* rankingPage;
+    MiddlePage* middlePage;
+
+    QStackedWidget* stackedWidget;
+
+    int FPS = 100;
+
+    void initUI();
+    void initConnections();
     void applyTheme(QString theme);
 
-    // 页面指针
-    StartPage *startPage;
-    SettingPage *settingPage;
-    RankingPage *rankingPage;
-    GamePage *gamePage;
-    MiddlePage *middlePage;
-    DifficultyPage*difficultyPage;
-    // 音频播放相关成员
-    QMediaPlayer *mediaPlayer;
-    QAudioOutput *audioOutput;
 };
-
+#endif // MAINWINDOW_H
